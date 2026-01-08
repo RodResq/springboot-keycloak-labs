7eeb49e6ed0d5a6236c70a3c9cca0338b7897159
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2026-01-08 15:02:33 -0300

    factor: - Adicionar Representação de ClientMapping

---
+++ src/main/java/br/jus/tjpa/jsecurity/AbstractClientMappingsRepresentation.java
@@ -0,0 +1,7 @@
+package br.jus.tjpa.jsecurity;
+
+import org.keycloak.representations.idm.ClientMappingsRepresentation;
+
+public abstract  class AbstractClientMappingsRepresentation extends AbstractArtifactConfiguration<ClientMappingsRepresentation> {
+    
+}
