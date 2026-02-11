637c45518e644e1dfbed6545d6e6d3f44dc48170
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2026-02-11 14:34:57 -0300

    refactor: - remove comentarios desnecessários 6

--- src/main/java/br/jus/tjpa/jsecurity/service/SecurityService.java
+++ src/main/java/br/jus/tjpa/jsecurity/service/SecurityService.java
@@ -10,102 +10,32 @@ import org.keycloak.representations.idm.authorization.*;
 
 public interface SecurityService {
 
-	/**
-	 * Método registrador de Realm.
-	 * 
-	 * @param representation {@link RealmRepresentation}
-	 */
 	boolean register(RealmRepresentation representation);
 
-	/**
-	 * Método registrador do Client.
-	 * 
-	 * @param representation {@link ClientRepresentation}
-	 */
 	boolean register(ClientRepresentation representation);
 
-	/**
-	 * Método registrador de Resource.
-	 * 
-	 * @param keycloakResource {@link ResourceRepresentation}
-	 */
 	boolean register(ResourceRepresentation representation);
 
-	/**
-	 * Método registrador de Policy do tipo Client.
-	 * 
-	 * @param representation {@link ClientPolicyRepresentation}
-	 */
 	boolean register(ClientPolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy Role.
-	 * 
-	 * @param representation {@link RolePolicyRepresentation}
-	 */
 	boolean register(RolePolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy do tipo Group.
-	 * 
-	 * @param representation {@link GroupPolicyRepresentation}
-	 */
 	boolean register(GroupPolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy do Tipo Javascript.
-	 * 
-	 * @param representation {@link JSPolicyRepresentation}
-	 */
 	boolean register(JSPolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy Rule.
-	 * 
-	 * @param representation {@link PolicyRepresentation}
-	 */
 	boolean register(PolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy Time.
-	 * 
-	 * @param representation {@link TimePolicyRepresentation}
-	 */
 	boolean register(TimePolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy User.
-	 * 
-	 * @param representation {@link UserPolicyRepresentation}
-	 */
 	boolean register(UserPolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Policy do tipo Aggregate.
-	 * 
-	 * @param representation {@link AggregatePolicyRepresentation}
-	 */
 	boolean register(AggregatePolicyRepresentation representation);
 
-	/**
-	 * Método registrador de Permission.
-	 * 
-	 * @param representation {@link ResourcePermissionRepresentation}
-	 */
 	boolean register(ResourcePermissionRepresentation representation);
 
-	/**
-	 * Método registrador de User.
-	 * 
-	 * @param representation
-	 */
 	boolean register(UserRepresentation representation);
 
-	/**
-	 * Retorna o ClientResource com clientId.
-	 * 
-	 * @return
-	 */
 	ClientResource getClientResource(String clientId);
 
 	AccessTokenResponse login(String username, String password);
